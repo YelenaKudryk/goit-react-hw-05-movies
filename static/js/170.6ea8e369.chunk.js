91d148f64d2a48bf0f8f734a3b6ad16e16ff4000
YelenaKudryk <106627951+YelenaKudryk@users.noreply.github.com>
2023-02-21 13:39:06 +0000

    Deploying to gh-pages from @ YelenaKudryk/goit-react-hw-05-movies@08238de701845b9d411246920b3c80d1bb6ed507 🚀

---
+++ static/js/170.6ea8e369.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[170],{7170:function(e,n,t){t.r(n),t.d(n,{default:function(){return o}});var r,a=t(168),c=t(3081).Z.h5(r||(r=(0,a.Z)(["\n  text-align: center;\n  padding-top: 100px;\n"]))),u=t(184),o=function(){return(0,u.jsx)(u.Fragment,{children:(0,u.jsx)(c,{children:"Page not found"})})}}}]);
+//# sourceMappingURL=170.6ea8e369.chunk.js.map
